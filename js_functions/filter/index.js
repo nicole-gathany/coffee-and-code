6b408e03be2c4474c2ae98f753d77a0bf6f69be5
nicole-gathany <nicole.gathany@gmail.com>
2021-04-15 20:21:31 -0400

    biance fixed the issue in the problem, it works on replit and in browser

--- js_functions/filter/index.js
+++ js_functions/filter/index.js
@@ -1,22 +1,20 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
-//works on browser but not replt for some reason
-
 function filter(array, callback) {
     // implement me
-    let array1 = array;
-    let result = []  
-    array1.forEach(function(element){
-        if(callback(element)===true){
-            result.push(element)
-        } 
-    }) 
-   return result;
+    let resultArray = [];
+    for(let i=0; i<array.length; i++){
+      if(callback(array[i])===true){
+        resultArray.push(array[i])
+      }
+    }
+    
+    return resultArray;
   }
   
   const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
   
-  const result = words.filter(word => word.length > 6);
+  const result = filter(words, word => word.length > 6);
   
   console.log(result);
   // expected output: Array ["exuberant", "destruction", "present"]
